714457cd5ef617ccba7748fe0afca5d2cda62ce0
William <williamwijaya.mail@gmail.com>
2022-04-05 00:22:51 +0000

    Added asssertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected){
+    console.log("🔴🔴🔴 Assertion Failed");
+  } else {
+    console.log("🟩🟩🟩 Assertion Passed");
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
