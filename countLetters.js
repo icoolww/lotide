01a1ef1ad58495b96a2c4472aad7ba04e99c7078
William <williamwijaya.mail@gmail.com>
2022-04-07 01:27:47 +0000

    Implement Count Letters

---
+++ countLetters.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("🔴🔴🔴 Assertion Failed");
+  } else {
+    console.log("🟩🟩🟩 Assertion Passed");
+  }
+};
+
+// output should be in objects, {}
+// create a loop to iterate each letter, for loops
+// count the letters, if statement
+
+const countLetters = function (string) {
+  let output = {}; // output in an empty objects
+  
+  for (let letter of string) {
+    // console.log(letter);
+    if (letter === " ") {
+      continue; // if there is space, continue the loop, but not printing empty space
+    } if (output[letter]) {
+      output[letter] += 1; // start the counting process
+    } else {
+      output[letter] = 1;
+    }
+  }
+  console.log(output);
+  return output;
+};
+
+
+countLetters("lighthouse in the house") 
+
+// expected output
+// {
+//   l: 1,
+//   i: 2,
+//   g: 1,
+//   h: 4,
+//   t: 2,
+//   o: 2,
+//   u: 2,
+//   s: 2,
+//   e: 3,
+//   n: 1,
+// }
