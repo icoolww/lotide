fc5fe8fe3deeb6b307086aadfdcb346ed4656293
William <williamwijaya.mail@gmail.com>
2022-04-08 05:03:05 +0000

    Implement Find Key

---
+++ findKey.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("🔴🔴🔴 Assertion Failed");
+  } else {
+    console.log("🟩🟩🟩 Assertion Passed");
+  }
+};
+
+const findKey = function (object, callback) {
+  let output = "";
+
+  for (let a of Object.keys(object)) { // iterate through the value
+
+    //  console.log(callback(object[a]));
+    if (callback(object[a])) {
+      output += a;
+      break;
+    }
+  }
+  console.log(output);
+  return output;
+};
+
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
+
+
+// const findKeyByValue = function (words, title) {
+//   let output = undefined;
+
+//   for (let a in words) { // iterate through the value
+//     // console.log(a);
+//     if (words[a] === title) {
+//       output = a;
+//     }
+//   }
+//   return output;
+// };
