8e92328dad1d7daa90a4c8da694b3c33d7091c80
William <williamwijaya.mail@gmail.com>
2022-04-11 23:46:55 +0000

    index.js with ES6 implementation

--- index.js
+++ index.js
@@ -5,42 +5,42 @@ const middle = require('./middle');
 
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
-
-const countLetters = require('./countLetters');
-const countOnly  = require('./countOnly');
 const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
 
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
+// const assertObjectsEqual = require('./assertObjectsEqual');
+// const countLetters = require('./countLetters');
+// const countOnly  = require('./countOnly');
+// const eqObjects = require('./eqObjects');
 
-const letterPositions = require('./letterPositions');
-const map = require('./map');
-const takeUntil = require('./takeUntil');
+// const findKey = require('./findKey');
+// const findKeyByValue = require('./findKeyByValue');
 
-const without = require('./without');
+// const letterPositions = require('./letterPositions');
+// const map = require('./map');
+// const takeUntil = require('./takeUntil');
+
+// const without = require('./without');
 
 
 
 module.exports = {
 
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map,
-  middle: middle,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays
   
 };
+
+// assertObjectsEqual: assertObjectsEqual,
+// countLetters: countLetters,
+// countOnly: countOnly,
+// eqObjects: eqObjects,
+// findKey: findKey,
+// findKeyByValue: findKeyByValue,
+// letterPositions: letterPositions,
+// map: map,
+// takeUntil: takeUntil,
+// without: without
