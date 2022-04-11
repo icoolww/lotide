970b0e0098be9ad18b28420992900c16afc20a9c
William <williamwijaya.mail@gmail.com>
2022-04-11 15:25:12 +0000

    head Refactor

--- head.js
+++ head.js
@@ -13,7 +13,8 @@ const head = function(arr) {
   return arr[0];
 };
 
+module.exports = head;
 
-// // TEST CODE
+// // // TEST CODE
 // assertEqual(head([5,6,7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
